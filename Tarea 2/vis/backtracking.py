ba4e0e5538011afbe6eaee9c2318e97c5eec216d
RicardoPayan <a218206581@unison.mx>
2023-03-30 08:28:47 -0700

    backtracking

--- Tarea 2/vis/backtracking.py
+++ Tarea 2/vis/backtracking.py
@@ -2,32 +2,42 @@ from search import SearchAlgorithm
 
 class Backtracking(SearchAlgorithm):
     def __init__(self, problem):
-        super().__init__(problem)
-        self.path = []
+        super().__init__(problem)     
+        self.path = []   
         self.backrefs = {}
+        # Se crea un diccionario para almacenar los nodos visitados y sus respectivos predecesores.
 
     def stateCost(self, state):
         return self.pastCosts.get(state, None)
-    
+        # Devuelve el costo acumulado de un estado si este ha sido visitado previamente, de lo contrario devuelve None.
+
+        
+    def path(self, state):
+        path = []
+        while state != self.problem.startState():
+            _, prevState = self.backrefs[state]
+            path.append(state)
+            state = prevState
+        return path
+
     def step(self):
         problem = self.problem
-        startState = self.startState
+        startState = self.startState        
         path = self.path
         pastCosts = self.pastCosts
         backrefs = self.backrefs
-        pastCosts = self.pastCosts
-        
 
         if self.actions:
             return self.path(problem.endState())
-
+            # Si ya se encontró la solución, se devuelve el camino desde el estado inicial hasta el estado final.
+       
         if not path:
             path.append(startState)
 
         state = path[-1]
         if problem.isEnd(state):
             self.actions = []
-            while state != startState:
+            while state == startState:
                 action, prevState = backrefs[state]
                 self.actions.append(action)
                 state = prevState
@@ -41,5 +51,6 @@ class Backtracking(SearchAlgorithm):
                 pastCosts[newState] = pastCosts.get(state,0) + cost
                 return path
 
-        path.pop()
+        if len(path) != 0:
+            path.pop()
         return path
